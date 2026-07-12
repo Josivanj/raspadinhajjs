ac555f24df4f032a87993b6ae7c670f7312a8bb7
Josivanl <josivanl@github.com>
2026-07-12 17:21:18 -0300

    Handle existing games table during migration

--- database/migrations/2023_10_07_183922_create_games_table.php
+++ database/migrations/2023_10_07_183922_create_games_table.php
@@ -11,6 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (Schema::hasTable('games')) {
+            return;
+        }
+
         Schema::create('games', function (Blueprint $table) {
             $table->id();
             $table->integer('provider_id')->unsigned()->index();
